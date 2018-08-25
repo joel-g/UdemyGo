ed0deab1f00041600a407c86411045278c004c69
Joel Guerra <j0g02ab@homeoffice.wal-mart.com>
2018-08-25 11:20:38 -0700

    structs

---
+++ structs/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jimPointer := &jim
+	fmt.Println(&jimPointer)
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%v+", p)
+}
