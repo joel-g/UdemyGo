2f0283fb196c3c133b5189aa104c0c5538903bf2
joel-g <joelaguerra1@gmail.com>
2018-08-26 22:29:05 -0700

    bot interfaces

---
+++ interfaces/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// very custom logic for english
+	return "Hello!"
+}
+
+func (spanishBot) getGreeting() string {
+	// very custom logic for spanish
+	return "Hola!"
+}
